afe9a25101ffb96d26048278e668a39a9f440548
Zeeshan Hayat <zeenux@gmail.com>
2018-10-07 16:25:27 +0500

    With Notes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,23 @@ class HelloWorld{
 
 public static void main(String [] args)
 {
+	    Double [] arr=new Double[1000000];
+			Double [] arr1=new Double[1000000];
+			Double [] arr2=new Double[1000000];
+			Double [] arr3=new Double[1000000];
+			Double [] arr4=new Double[1000000];
+			Double [] arr5=new Double[1000000];
+			Double [] arr6=new Double[1000000];
+			Double [] arr7=new Double[1000000];
+			Double [] arr8=new Double[1000000];
 
+			Double [] arr9=new Double[1000000];
+			Double [] arr10=new Double[1000000];
+
+			Double [] arr11=new Double[1000000];
+			Double [] arr12=new Double[1000000];
+			Double [] arr13=new Double[1000000];
+			Double [] arr15=new Double[1000000];
 			System.out.println("Hello Folks. 2+2 is "+(2+2));
 
 }
